f51e76d489d70fb6bae63a564af70762bd8857f7
Zoma <hazem_ahmed_93@yahoo.com>
2020-05-27 18:41:49 +0200

    Updates

---
+++ precache-manifest.2946be8c29aa638b12f3fe08a3335b25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d7f8b96d15cd9782366e837f5b15b20",
+    "url": "/GuestBook-FE/index.html"
+  },
+  {
+    "revision": "4f7e4df13ad9c922e635",
+    "url": "/GuestBook-FE/static/css/main.176838d8.chunk.css"
+  },
+  {
+    "revision": "1a92c4dbbedeb3de4ea1",
+    "url": "/GuestBook-FE/static/js/2.177bd923.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GuestBook-FE/static/js/2.177bd923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7e4df13ad9c922e635",
+    "url": "/GuestBook-FE/static/js/main.8935a09c.chunk.js"
+  },
+  {
+    "revision": "efc87f199895dd7df8cc",
+    "url": "/GuestBook-FE/static/js/runtime-main.53e311e6.js"
+  }
+]);
